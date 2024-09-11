c274cc38910069ab60401f8846ad30313d474a13
abiyasha031104 <abiyasha626@gmail.com>
2024-09-11 14:17:44 +0700

    Praktikum 2a - Implementasi Input dan Output

--- keempat.php
+++ keempat.php
@@ -5,7 +5,7 @@ class Author
     public $name;
     public $description;
 
-    public function __construct($name, $description)
+    public function __construct($name, $description)                                                                                                                                                                                                                                                                                                                                                                           
     {
         $this->name = $name;
         $this->description = $description;
